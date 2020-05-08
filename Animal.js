46cb4d03fdc5a8cdd245f7ddf882900db733e068
fez <mshengu11@gmail.com>
2020-05-08 18:16:34 +0200

    Changed everything to be in one file.

--- Animal.js
+++ Animal.js
@@ -12,4 +12,67 @@ class Animal {
   }
 }
 
-module.exports = { Animal };
+class Cat extends Animal {
+  constructor(name) {
+    super(name);
+    this.name = name;
+  }
+
+  sound() {
+    Animal.prototype.sound.call(this, "Cats ", "meows");
+  }
+
+  eat() {
+    Animal.prototype.eat.call(this);
+  }
+}
+
+class Dog extends Animal {
+  constructor(name) {
+    super(name);
+    this.animalSound = "bark";
+    this.animalType = "Dogs";
+  }
+
+  sound() {
+    Animal.prototype.sound.call(this, this.animalType, this.animalSound);
+  }
+
+  eat() {
+    Animal.prototype.eat.call(this);
+  }
+}
+
+class Home {
+  constructor() {
+    this.allAnimals = [];
+  }
+
+  adoptPet(animalType) {
+    var animalExists = false;
+    var i = 0;
+    while (animalExists == false && i < this.allAnimals.length) {
+      if (animalType != this.allAnimals[i]) {
+        animalExists = false;
+      } else {
+        console.log("Not ok at all");
+        animalExists = true;
+        break;
+      }
+      i++;
+    }
+
+    if (animalExists == false) {
+      this.allAnimals.push(animalType);
+      console.log("Totally okay");
+    }
+  }
+
+  makeAllSounds() {
+    for (var i = 0; i < this.allAnimals.length; i++) {
+      this.allAnimals[i].sound();
+    }
+  }
+}
+
+var home = new Home();
